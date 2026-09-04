f9ba7c6b8f8a5c139d44352e4ac239cf77f9da5f
Ryan-Lee777 <a437183824@gmail.com>
2026-09-04 03:42:39 +0000

    [skip actions] lab/lab01/ch01_t09_numbers.py

--- lab/lab01/ch01_t09_numbers.py
+++ lab/lab01/ch01_t09_numbers.py
@@ -1,8 +1,8 @@
 # 青瓜数量：
-cucumbers = 1
+CUCUMBERS = 1
 #青瓜单价：
 price_per_cucumber = 3.25
 #总价：
-total_cost = cucumbers*price_per_cucumber
-
-print=(t)
+total_cost = cucumbers * price_per_cucumber
+#显示：
+print(total_cost)
