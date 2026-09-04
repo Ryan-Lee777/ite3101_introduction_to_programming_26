a88fc24571885044663594c2bfb1a5f9e0b99f99
Ryan-Lee777 <a437183824@gmail.com>
2026-09-04 02:56:35 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -11,6 +11,6 @@ september_rainfall = 5.16
 annual_rainfall += september_rainfall
 
 october_rainfall = 7.20
-annual_rainfall
+annual_rainfall += oct
 november_rainfall = 5.06
 december_rainfall = 4.06
