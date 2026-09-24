6371302c86505b30b433dee3a7dc41b821a46016
Ryan-Lee777 <a437183824@gmail.com>
2026-09-24 08:28:47 +0000

    [skip actions] lab/lab04/ch04_t14_elif.py

--- lab/lab04/ch04_t14_elif.py
+++ lab/lab04/ch04_t14_elif.py
@@ -3,10 +3,7 @@ def greater_less_equal_5(answer: int) -> int:
         return 'A'
     elif answer >= 80:
         return 'B'
-    elif answer >= 70:
-        return 'C'
-    elif answer >= 65:
-        return 'D'
+
     else:
         return 'F'
 
