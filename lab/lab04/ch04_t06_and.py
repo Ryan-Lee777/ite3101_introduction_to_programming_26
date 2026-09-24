ef9516356bd5c55c69d896258f116cb95dd25d7b
Ryan-Lee777 <a437183824@gmail.com>
2026-09-24 07:23:25 +0000

    [skip actions] lab/lab04/ch04_t07_or.py

--- lab/lab04/ch04_t06_and.py
+++ lab/lab04/ch04_t06_and.py
@@ -7,3 +7,5 @@ bool_three = 19 % 4 != 300 / 10 / 10 and False
 bool_four = -(1 ** 2) < 2 ** 0 and 10 % 10 <= 20 - 10 * 2
 
 bool_five = True and True
+
+print(bool_one, bool_two, bool_three, bool_four, bool_five)
