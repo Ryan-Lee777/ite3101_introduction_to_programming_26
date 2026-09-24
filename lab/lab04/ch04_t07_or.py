cfd401c594eb1f2531bd95ad545bd94d4dfd5259
Ryan-Lee777 <a437183824@gmail.com>
2026-09-24 07:27:03 +0000

    [skip actions] lab/lab04/ch04_t08_not.py

--- lab/lab04/ch04_t07_or.py
+++ lab/lab04/ch04_t07_or.py
@@ -7,3 +7,5 @@ bool_three = 100 ** 0.5 >= 50 or False
 bool_four = True or True
 
 bool_five = 1 ** 100 == 100 ** 1 or 3 * 2 * 1 != 3 + 2 + 1
+
+print(bool_one, bool_two, bool_three, bool_four, bool_five)
